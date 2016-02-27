5f9148d6f947c8cdb81a17574f5e0090aaf67bc4
Andres <aguilarandres296@gmail.com>
2016-02-26 22:38:04 -0600

    package algoritmos

---
+++ src/Algoritmos/FCFS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algoritmos;
+import static simulacion.so.FXMLDocumentController.colaProcesos;
+import 
+
+/**
+ *
+ * @author Andres
+ */
+public class FCFS {
+     
+    public void algoritmo(){
+        
+        if(!colaProcesos.isEmpty()&& ){
+            
+        }
+    }
+}
