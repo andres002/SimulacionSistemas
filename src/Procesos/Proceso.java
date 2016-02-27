1533966b690089329f962dde660038c42180def3
Andres <aguilarandres296@gmail.com>
2016-02-26 19:18:14 -0600

    pavkage proceso

---
+++ src/Procesos/Proceso.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Procesos;
+
+/**
+ *
+ * @author Andres
+ */
+public class Proceso {
+    public int tiempo = 0;
+    public int prioridad = 0;
+    
+}
