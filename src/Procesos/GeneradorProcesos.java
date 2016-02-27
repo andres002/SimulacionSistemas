3f74291ecf129b055a34a6cbac1393a7885f46d6
Andres <aguilarandres296@gmail.com>
2016-02-26 20:25:35 -0600

    procesos

--- src/Procesos/GeneradorProcesos.java
+++ src/Procesos/GeneradorProcesos.java
@@ -15,15 +15,40 @@ import static simulacion.so.FXMLDocumentController.colaProcesos;
 public class GeneradorProcesos implements Runnable{
     
     final double probabilidad =  0.9502;
+    int cont = 0;
    
    
     
     public void creaProcesos(){
            Random rnd = new Random();
-            double posible = rnd.nextDouble();
-            double posibleTipo = rnd.nextDouble();
+            double posible = rnd.nextDouble();      //posibilidad de que se cree un proceso
+            int posibleTick = rnd.nextInt(10)+1;  // posibilidad de ticks
+            double posibleTipo = rnd.nextDouble();  // posibilidad de un tipo de proceso
+            int posiblePrioriodad;
+            cont++;
             if(posible < probabilidad){
-                colaProcesos.add(new Proceso());
+                if(posibleTipo <=0.25){    // Procesos de Sistema
+                    
+                    posiblePrioriodad = rnd.nextInt(4)+1;
+                    colaProcesos.add(new Proceso(posibleTick,0,posiblePrioriodad,"P" + cont));
+                    
+                }else if(posibleTipo <= 0.50){ //Procesos interactivos
+                    
+                    posiblePrioriodad = rnd.nextInt(2)+1;
+                    colaProcesos.add(new Proceso(posibleTick,1,posiblePrioriodad,"P" + cont));
+                    
+                }else if(posibleTipo <= 0.75){ // Procesos de edicion
+                    
+                    posiblePrioriodad = rnd.nextInt(2)+1;
+                    colaProcesos.add(new Proceso(posibleTick,2,posiblePrioriodad,"P" + cont));
+                    
+                }else if(posibleTipo <= 1){ //Proceso por lotes
+                    
+                    posiblePrioriodad = rnd.nextInt(2)+1;
+                    colaProcesos.add(new Proceso(posibleTick,3,posiblePrioriodad,"P" + cont));
+                }
+                
+                
             }
                 
            
