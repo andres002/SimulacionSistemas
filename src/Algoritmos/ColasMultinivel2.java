7acc697ae843aedb2f86b429e077a06a57bbe9bc
Andres <aguilarandres296@gmail.com>
2016-03-04 06:11:05 -0600

    casi listos colas Multinivel2

--- src/Algoritmos/ColasMultinivel2.java
+++ src/Algoritmos/ColasMultinivel2.java
@@ -55,11 +55,13 @@ public class ColasMultinivel2 implements Runnable {
             try {
                 q = Integer.parseInt(quantum);
                 Core c = new Core();
-                if (!colaProcesos.isEmpty()) {
+                
                     while (!bandera) {
                         System.out.println("haz nada");
                     }
+                    bandera  = false;
                     while (!colaProcesos.isEmpty()) {
+                        bandera = true;
                         while (!bandera) {
                             System.out.println("haz nada");
                         }
@@ -67,30 +69,37 @@ public class ColasMultinivel2 implements Runnable {
                         Proceso aux = colaProcesos.get(0);
                         bandera = true;
                         int tipo = aux.tipo;
-                        switch(tipo){
+                        switch (tipo) {
                             case 0:
-                                tipo_0.add(aux);                             
-                            break;
+                                tipo_0.add(aux);
+                                break;
                             case 1:
                                 tipo_1.add(aux);
-                            break;
+                                break;
                             case 2:
                                 tipo_2.add(aux);
-                            break;
+                                break;
                             case 3:
                                 tipo_3.add(aux);
-                            break;
+                                break;
                             default:
                                 System.out.println("error, no debería entrar aqui");
                                 break;
                         }
-                        colaProcesos.remove(0);
                         while (!bandera) {
                             System.out.println("haz nada");
                         }
+                        bandera = false;
+                        colaProcesos.remove(0);
+                        bandera = true;
+                        while (!bandera) {
+                            System.out.println("haz nada");
+                        }
+                        bandera = false;
                     }
-                    
-                    if(!tipo_0.isEmpty()){
+                    bandera = true;
+
+                    if (!tipo_0.isEmpty()) {
                         Proceso aux = tipo_0.get(0);
                         if ((aux.ticks - q) > 0) {
                             int te = t_procesador - aux.llegada;
@@ -114,7 +123,6 @@ public class ColasMultinivel2 implements Runnable {
                             bandera = true;
                             tipo_0.remove(0);
 
-                            
                         } else {
                             System.out.println("entra al elseeeeeeeeeeeeee");
                             Resultados = aux.nombre + ": TE: " + (t_procesador - aux.llegada) + " Ticks: " + aux.ticks
@@ -130,16 +138,91 @@ public class ColasMultinivel2 implements Runnable {
                             //Resultados = aux.nombre + ": TE: " + te + " Ticks: " + aux.ticks
                             //      + " Llegada: " + aux.llegada;
                             //System.out.println("\n"+Resultados);
-                            numProcesos++;                            
+                            numProcesos++;
                             tipo_0.remove(0);
-                        } 
-                    }else if(!tipo_1.isEmpty()){
+                        }
+                    } else if (!tipo_1.isEmpty()) {
+                        Proceso aux, aux2;
+                        aux = tipo_1.get(0);
+                        int temp = aux.prioridad;
+                        int limite = tipo_1.size();
+
+                        for (recorrido = 1; recorrido < limite; recorrido++) {
+                            aux2 = tipo_1.get(recorrido);
+                            if (aux2.prioridad < aux.prioridad) {
+                                aux = aux2;
+                            }
+                        }
+                        while (!bandera) {
+                            System.out.println("haz nada");
+                        }
+                        try {
+                            int te = cont - aux.llegada;
+                            double tr = te + aux.ticks;
+                            double tp = (te + aux.ticks) / aux.ticks.doubleValue();
+                            System.out.println("Valor de tp " + tp);
+                            Te = Te + te;
+                            numProcesos++;
+                            Resultados = aux.nombre + ": TE: " + te + " Ticks: " + aux.ticks + "Prioridad: " + aux.tipo
+                                    + " Llegada: " + aux.llegada + " Tr: " + tr + " Tp: "
+                                    + tp;
+                            cont_ticks += aux.ticks;
+                            Tr += te + aux.ticks;
+                            System.out.println("\n" + Resultados);
+                            Thread.sleep(1000 * aux.ticks);
+                            c.procesar(aux, aux.ticks);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Prioridad.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                         
+                        tipo_1.remove(aux);
+                    }else if(!tipo_2.isEmpty()){
+                        double te = cont-tipo_2.get(0).llegada;
+                        double tr = te + tipo_2.get(0).ticks;
+                        double tp = (te + tipo_2.get(0).ticks) / tipo_2.get(0).ticks;
+                        Te = Te + te;
+                        numProcesos++;
+
+                        Resultados = tipo_2.get(0).nombre + ": TE: " + te + " Ticks: " + tipo_2.get(0).ticks
+                                + " Llegada: " + tipo_2.get(0).llegada + " Tr: "+ tr + " Tp: "
+                                + tp;
+                        Tr += te + tipo_2.get(0).ticks;
+                        Tp += (te + tipo_2.get(0).ticks) / tipo_2.get(0).ticks;
+                        System.out.println("\n" + Resultados);
+                        System.out.println("aqui imprimimos el TIIIIIIIIICKS ---  " + tipo_2.get(0).ticks);
+                        Thread.sleep(1000 * tipo_2.get(0).ticks);
+                        System.out.println("despues de dormir");
+                        //fx.setResultados();
+                        cont_ticks += tipo_2.get(0).ticks;
+                        c.procesar(tipo_2.get(0), tipo_2.get(0).ticks);
+                        tipo_2.remove(0);
+                    }else if(!tipo_3.isEmpty()){
+                        double te = cont-tipo_3.get(0).llegada;
+                        double tr = te + tipo_3.get(0).ticks;
+                        double tp = (te + tipo_3.get(0).ticks) / tipo_3.get(0).ticks;
+                        Te = Te + te;
+                        numProcesos++;
+
+                        Resultados = tipo_3.get(0).nombre + ": TE: " + te + " Ticks: " + tipo_3.get(0).ticks
+                                + " Llegada: " + tipo_3.get(0).llegada + " Tr: "+ tr + " Tp: "
+                                + tp;
+                        Tr += te + tipo_3.get(0).ticks;
+                        Tp += (te + tipo_3.get(0).ticks) / tipo_3.get(0).ticks;
+                        System.out.println("\n" + Resultados);
+                        System.out.println("aqui imprimimos el TIIIIIIIIICKS ---  " + tipo_3.get(0).ticks);
+                        Thread.sleep(1000 * tipo_3.get(0).ticks);
+                        System.out.println("despues de dormir");
+                        //fx.setResultados();
+                        cont_ticks += tipo_3.get(0).ticks;
+                        c.procesar(tipo_3.get(0), tipo_3.get(0).ticks);
+                        tipo_3.remove(0);
                     }
-                } else if (parar) {
-                    System.out.println("Se terminaron los procesos");
-                    pararproceso = true;
-                    return;
+                    if(tipo_0.isEmpty() && tipo_1.isEmpty()&& tipo_2.isEmpty() && tipo_3.isEmpty()){
+                        if (parar) {
+                        System.out.println("Se terminaron los procesos");
+                        pararproceso = true;
+                        return;
+                        }
                 }
 
             } catch (InterruptedException ex) {
